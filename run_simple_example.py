5887edf0cf01a0d8aaa4e15c3643cb19af8dafdf
Ana Gainaru <ana.gainaru@gmail.com>
2019-04-09 13:04:26 -0400

    Made the example code self explanatory

--- run_simple_example.py
+++ run_simple_example.py
@@ -6,33 +6,37 @@ import sys
 import numpy as np
 
 
-def run_scenario(procs, apl):
+def run_scenario(num_procssing_units, job_list):
     simulator = Simulator.Simulator(check_correctness=True,
                                     generate_gif=True,
                                     output_file_handler=sys.stdout)
     sch = Scheduler.BatchScheduler(System(procs))
-    simulator.create_scenario("test_batch", sch, 1.5, job_list=apl)
+    simulator.create_scenario("test_batch", sch, 1.5, job_list=job_list)
     simulator.run()
 
     sch = Scheduler.OnlineScheduler(System(procs))
-    simulator.create_scenario("test_online", sch, 1.5, job_list=apl)
+    simulator.create_scenario("test_online", sch, 1.5, job_list=job_list)
     simulator.run()
 
 
 if __name__ == '__main__':
-    procs = 10
-    apl = set()
+    num_procssing_units = 10
+    job_list = set()
     for i in range(10):
         execution_time = np.random.randint(11, 100)
-        apl.add(ApplicationJob(np.random.randint(1, 11),
-                               0,
+        request_time = execution_time + int(i / 2) * 10
+        processing_units = np.random.randint(
+            1, num_processing_units + 1)
+        submission_time = 0
+        job_list.add(ApplicationJob(processing_units,
+                               submission_time,
                                execution_time,
-                               execution_time + int(i / 2) * 10,
+                               request_time,
                                i))
-    apl.add(ApplicationJob(np.random.randint(9, 11), 0, 100, 90, 10))
+    job_list.add(ApplicationJob(np.random.randint(9, 11), 0, 100, 90, 10))
 
     print("Scenario : makespan : utilization : average_job_utilization : "
           "average_job_response_time : average_job_stretch : "
           "average_job_wait_time : failures")
 
-    execution_log = run_scenario(procs, apl)
+    run_scenario(num_procssing_units, job_list)
