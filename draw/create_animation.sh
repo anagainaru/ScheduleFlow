1fed5fc5575ea4979c1d11a755fd190b45286a07
Ana Gainaru <ana.gainaru@gmail.com>
2019-05-13 19:23:43 +0000

    Temporary compilation files created by latex should be stored in draw before they are deleted

--- draw/create_animation.sh
+++ draw/create_animation.sh
@@ -5,13 +5,13 @@ if [ "$#" -lt 1 ]; then
     exit
 fi
 
-for i in draw/$1_*.tex; do echo "Compile $i ..."; pdflatex $i > /dev/null; done
-rm $1_*.{aux,log,nav,out,snm,toc}
+for i in draw/$1_*.tex; do echo "Compile $i ..."; pdflatex -output-directory draw $i > /dev/null; done
+rm draw/$1_*.{aux,log,nav,out,snm,toc}
 echo "Generating GIF ..."
-total_files=`ls -l $1_*.pdf | wc -l`
-convert -loop 1 -delay 200 -quality 100 -density 250 `for ((i=0; i<${total_files}; i++)); do echo "$1_$i.pdf"; done` draw/$1.gif
+total_files=`ls -l draw/$1_*.pdf | wc -l`
+convert -loop 1 -delay 200 -quality 100 -density 250 `for ((i=0; i<${total_files}; i++)); do echo "draw/$1_$i.pdf"; done` draw/$1.gif
 echo  "Removing unecessary files ..."
-rm $1_*.pdf
+rm draw/$1_*.pdf
 if [ $2 == "delete" ]; then
 	rm draw/$1_*.tex
 fi
