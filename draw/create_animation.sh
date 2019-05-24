cd4306be977edd28e8b23d3f129e534fc8b27fbf
Ana Gainaru <ana.gainaru@gmail.com>
2019-05-24 23:04:19 +0000

    Small bug fix

--- draw/create_animation.sh
+++ draw/create_animation.sh
@@ -9,7 +9,7 @@ for i in $ScheduleFlow_PATH/draw/$1_*.tex; do echo "Compile $i ..."; pdflatex -o
 rm $ScheduleFlow_PATH/draw/$1_*.{aux,log,nav,out,snm,toc}
 echo "Generating GIF ..."
 total_files=`ls -l $ScheduleFlow_PATH/draw/$1_*.pdf | wc -l`
-convert -loop 1 -delay 200 -quality 100 -density 250 `for ((i=0; i<${total_files}; i++)); do echo "$ScheduleFlow_PATH/draw/$1_$i.pdf"; done`$SF_DRAW_PATH/draw/$1.gif
+convert -loop 1 -delay 200 -quality 100 -density 250 `for ((i=0; i<${total_files}; i++)); do echo "$ScheduleFlow_PATH/draw/$1_$i.pdf"; done` $SF_DRAW_PATH/draw/$1.gif
 echo  "Removing unecessary files ..."
 rm $ScheduleFlow_PATH/draw/$1_*.pdf
 if [ $2 == "delete" ]; then
