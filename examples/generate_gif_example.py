1da2eaa0f9a48bfc822644ea06a602bfc6f394de
Ana Gainaru <ana.gainaru@gmail.com>
2025-04-24 11:08:20 -0400

    Changes to the example script to plot both gif and jpgs

--- examples/generate_gif_example.py
+++ examples/generate_gif_example.py
@@ -4,24 +4,10 @@ import ScheduleFlow
 import numpy as np
 import os
 
-def run_scenario(num_processing_units, job_list):
-    simulator = ScheduleFlow.Simulator(check_correctness=True,
-                                       generate_gif=True,
-                                       output_file_handler=sys.stdout)
-    sch = ScheduleFlow.Scheduler(
-        ScheduleFlow.System(num_processing_units))
-    simulator.create_scenario(sch, job_list=job_list,
-                              scenario_name="test_batch")
-    execution = simulator.run(metrics="execution_log")
-    print(execution)
-    
-if __name__ == '__main__':
-    os.environ["ScheduleFlow_PATH"] = ".."
-    num_processing_units = 10
-    
+def run_scenario(num_processing_units, num_jobs):
     job_list = set()
     # create the list of applications
-    for i in range(10):
+    for i in range(num_jobs):
         execution_time = np.random.randint(1800, 10000)
         request_time = execution_time + int(i / 2) * 1500
         processing_units = np.random.randint(
@@ -34,6 +20,27 @@ if __name__ == '__main__':
             [request_time], name="J"+str(i)))
     # add a job that request less time than required for its first run
     job_list.add(ScheduleFlow.Application(np.random.randint(9, 11), 0,
-                                          5000, [4000, 5500], name="J10"))
+                                          5000, [4000, 5500],
+                                          name="J"+str(i)))
+    generate_gif = False
+    generate_jpg = False
+    if num_jobs < 20:
+        generate_gif = True
+    if num_jobs > 90:
+        generate_jpg = True
+    simulator = ScheduleFlow.Simulator(check_correctness=True,
+                                       generate_gif=generate_gif,
+                                       generate_jpg=generate_jpg,
+                                       output_file_handler=sys.stdout)
+    sch = ScheduleFlow.Scheduler(
+        ScheduleFlow.System(num_processing_units))
+    simulator.create_scenario(sch, job_list=job_list,
+                              scenario_name="test_jpg")
+    execution = simulator.run(metrics="execution_log")
+    # print(execution)
 
-    run_scenario(num_processing_units, job_list)
+if __name__ == '__main__':
+    os.environ["ScheduleFlow_PATH"] = ".."
+    num_processing_units = 10
+    run_scenario(num_processing_units, 10)
+    run_scenario(num_processing_units, 100)
